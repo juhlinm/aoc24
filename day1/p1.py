7bdcbd5c52c0b9c63eff3403474a4746ecd64b82
Mattias Juhlin <mattias@juhlin.de>
2024-12-01 08:24:30 +0100

    Day1

---
+++ day1/p1.py
@@ -0,0 +1,18 @@
+with open("day1/inp.txt", "r") as f:
+    lines = f.readlines()
+
+l1 = []
+l2 = []
+for l in lines:
+    l = l.strip()
+    l1.append(int(l.split(" ")[0]))
+    l2.append(int(l.split(" ")[-1]))
+
+l1 = sorted(l1)
+l2 = sorted(l2)
+
+sum = 0
+for x, y in zip(l1, l2):
+    sum += abs(x-y)
+
+print(sum)
