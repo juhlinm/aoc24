aaad34fab43adc309c9083051d68893bb5dd6b13
Mattias Juhlin <mattias@juhlin.de>
2024-12-10 21:01:19 +0100

    Day10 p2 WIP

--- day10/p1.py
+++ day10/p1.py
@@ -22,10 +22,10 @@ def is_next(a, b):
     return (abs(a[0] - b[0]) == 1 and a[1] == b[1]) or (abs(a[1] - b[1]) == 1 and a[0] == b[0])
 
 
-def paths(a, nums, ls):
+def paths(a, nums, ls, num):
     cnt = 0
-    for n in nums:
-        if is_next(a, n) and n not in ls:
+    for n_pos in nums:
+        if is_next(a, n_pos) and (n_pos, num) not in ls:
             cnt += 1
     if cnt > 1:
         return a
@@ -35,48 +35,47 @@ def paths(a, nums, ls):
 
 def walk(pos, ls, nums, pcs, num):
     for n_pos in nums:
-        if is_next(pos, n_pos) and n_pos not in ls:
-            p = paths(pos, nums, ls)
+        if is_next(pos, n_pos) and (n_pos, num) not in ls:
+            p = paths(pos, nums, ls, num)
             if p:
-                pcs.append((p, num))
-            ls.append(n_pos)
-            break
-    return n_pos, ls, pcs
+                pcs.extend([(p, num) for _ in range(3)])
+            ls.append((n_pos, num))
+            return n_pos, ls, pcs
+    return pos, ls, pcs
 
 
 def solve(prob):
-    res = 0
+    ress = []
     nums = {}
     for i in range(10):
         nums[i] = find_num(prob, i)
 
     for x0 in range(len(nums[0])):
+        res = set()
         ls = []
         pcs = []
         o_ls = [(-1, -1)]
         pos = nums[0][x0]
         i = 1
-        while ls != o_ls:
+        while ls != o_ls or len(pcs):
             o_pos = [(-1, -1)]
             while pos != o_pos and i < 10:
                 o_pos = pos
                 o_ls = ls.copy()
                 pos, ls, pcs = walk(pos, ls, nums[i], pcs, i)
                 if i == 9 and pos != o_pos:
-                    res += 1
+                    res.add(pos)
                 i += 1
             if len(pcs):
-                print(ls)
-                print(pcs)
-                print("--------------")
                 pos, i = pcs.pop()
             else:
                 break
-    return res
+        ress.extend(list(res))
+    return len(ress)
 
 
 assert solve(two) == 2
 assert solve(three) == 3
-print(solve(four))
-# print(solve(threesix) == 36)
-# print(solve(inp))
+assert solve(threesix) == 36
+assert solve(four) == 4
+print(solve(inp))
--- day10/p2.py
+++ day10/p2.py
@@ -1,2 +1,90 @@
-with open("day10/inp.txt", "r") as f:
-    lines = f.readlines().strip()
+import sys
+
+sys.setrecursionlimit(10**6)
+
+twotwoseven = list(map(lambda x: x.strip(), open("day10/twop2.txt", "r").readlines()))
+three = list(map(lambda x: x.strip(), open("day10/threep2.txt", "r").readlines()))
+thirteen = list(map(lambda x: x.strip(), open("day10/four.txt", "r").readlines()))
+eightyone = list(map(lambda x: x.strip(), open("day10/threesix.txt", "r").readlines()))
+inp = list(map(lambda x: x.strip(), open("day10/inp.txt", "r").readlines()))
+
+
+def find_num(lines, num):
+    xs = []
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == str(num):
+                xs.append((x, y))
+    return xs
+
+
+def is_next(a, b):
+    return (abs(a[0] - b[0]) == 1 and a[1] == b[1]) or (abs(a[1] - b[1]) == 1 and a[0] == b[0])
+
+
+def paths(a, nums, ls, num):
+    n_poses = []
+    for n_pos in nums:
+        if is_next(a, n_pos) and (n_pos, num) not in ls:
+            n_poses.append((a, n_pos))
+    if len(n_poses) > 1:
+        return n_poses
+    else:
+        return None
+
+
+def walk(pos, ls, nums, pcs, num):
+    ppcs = [pc[0] for pc in pcs]
+    for n_pos in nums:
+        if is_next(pos, n_pos) and (pos, n_pos) not in ppcs:
+            _paths = paths(pos, nums, ls, num)
+            if _paths:
+                pcs.extend([(p[0], p[1], num) for p in _paths])
+                print([(p[0], p[1], p[2] - 1) for p in pcs])
+            ls.append((n_pos, num))
+            return n_pos, ls, pcs
+    return pos, ls, pcs
+
+
+def solve(prob):
+    ress = 0
+    nums = {}
+    for i in range(10):
+        nums[i] = find_num(prob, i)
+
+    for x0 in range(len(nums[0])):
+        res = []
+        ls = []
+        pcs = []
+        o_ls = [(-1, -1)]
+        pos = nums[0][x0]
+        i = 1
+        while ls != o_ls or len(pcs):
+            o_pos = [(-1, -1)]
+            while pos != o_pos and i < 10:
+                o_pos = pos
+                o_ls = ls.copy()
+                pos, ls, pcs = walk(pos, ls, nums[i], pcs, i)
+                if i == 9 and pos != o_pos:
+                    res.append(pos)
+                i += 1
+            if len(pcs):
+                pos, _, i = pcs.pop()
+                # print([(p[0][0], p[0][1] - 1) for p in pcs])
+            else:
+                break
+        ress += len(res) - 1
+    return ress
+
+
+# assert solve(three) == 3
+
+
+print(solve(thirteen))
+
+
+
+# assert solve(thirteen) == 13
+# assert solve(eightyone) == 81
+# assert solve(twotwoseven) == 227
+# print(solve(inp))
