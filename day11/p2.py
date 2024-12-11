858e9ea31b2681cb2a5c3111755e16873a9a8a36
MJUHLIN1 <mattias.juhlin@volvocars.com>
2024-12-11 18:44:31 +0100

    Day11 p2 WIP

--- day11/p2.py
+++ day11/p2.py
@@ -1,7 +1,5 @@
 import sys
 
-from line_profiler import profile
-
 sys.setrecursionlimit(10**6)
 
 seven = list(map(int, open("day11/seven.txt", "r").read().strip().split()))
@@ -9,54 +7,38 @@ twentytwo = list(map(int, open("day11/twentytwo.txt", "r").read().strip().split(
 inp = list(map(int, open("day11/inp.txt", "r").read().strip().split()))
 
 
-def count_digits(num):
-    if num == 0:
-        return 1
-
-    c = 0
-    while num > 0:
-        num //= 10
-        c += 1
-
-    return c
-
-
 def split_integer(num, c):
-    if num == 0:
-        return 0, 0
-
-    pwr = 10 ** (c // 2)
-    x = num // pwr
-    y = num % pwr
-
-    return x, y
+    return int(str(num)[: c // 2]), int(str(num)[c // 2 :])
 
 
-@profile
 def rool(inp):
     if inp == 0:
         return [1]
     else:
-        s = count_digits(inp)
-        if s % 2 == 0:
-            x, y = split_integer(inp, s)
+        c = len(str(inp))
+        if c % 2 == 0:
+            x, y = split_integer(inp, c)
             return [x, y]
         else:
             return [inp * 2024]
 
 
 def solve(prob, iter):
+    trans = {}
     for i in range(iter):
-        # print(i)
         o_prob = prob.copy()
         prob = []
         for x in o_prob:
-            prob.extend(rool(x))
-        # print(prob)
+            if x in trans:
+                prob.extend(trans[x])
+            else:
+                r = rool(x)
+                trans[x] = r
+                prob.extend(r)
     return len(prob)
 
 
-print(solve(seven, 1))
-print(solve(twentytwo, 6))
-print(solve(twentytwo, 25))
-# print(solve(inp, 75))
+print(solve(seven, 1), 7)
+print(solve(twentytwo, 6), 22)
+print(solve(twentytwo, 25), 55312)
+print(solve(inp, 75))
