992cbe8101a11790411f89154b45aead76f7a046
Mattias Juhlin <mattias@juhlin.de>
2024-12-12 22:47:02 +0100

    Day10

--- day10/p2.py
+++ day10/p2.py
@@ -1,7 +1,3 @@
-import sys
-
-sys.setrecursionlimit(10**6)
-
 twotwoseven = list(map(lambda x: x.strip(), open("day10/twop2.txt", "r").readlines()))
 three = list(map(lambda x: x.strip(), open("day10/threep2.txt", "r").readlines()))
 thirteen = list(map(lambda x: x.strip(), open("day10/four.txt", "r").readlines()))
@@ -9,82 +5,51 @@ eightyone = list(map(lambda x: x.strip(), open("day10/threesix.txt", "r").readli
 inp = list(map(lambda x: x.strip(), open("day10/inp.txt", "r").readlines()))
 
 
-def find_num(lines, num):
+def get_starts(lines):
     xs = []
     for y, line in enumerate(lines):
         for x, c in enumerate(line):
-            if c == str(num):
+            if c == "0":
                 xs.append((x, y))
     return xs
 
+def get_heightmap(lines):
+    hs = {}
+    for y in range(-1, len(lines) + 1):
+        for x in range(-1, len(lines[0]) + 1):
+            hs[(x,y)] = -2
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            try:
+                hs[(x,y)] = int(c)
+            except ValueError:
+                hs[(x,y)] = -2
+    return hs
 
-def is_next(a, b):
-    return (abs(a[0] - b[0]) == 1 and a[1] == b[1]) or (abs(a[1] - b[1]) == 1 and a[0] == b[0])
-
-
-def paths(a, nums, ls, num):
-    n_poses = []
-    for n_pos in nums:
-        if is_next(a, n_pos) and (n_pos, num) not in ls:
-            n_poses.append((a, n_pos))
-    if len(n_poses) > 1:
-        return n_poses
-    else:
-        return None
-
-
-def walk(pos, ls, nums, pcs, num):
-    ppcs = [pc[0] for pc in pcs]
-    for n_pos in nums:
-        if is_next(pos, n_pos) and (pos, n_pos) not in ppcs:
-            _paths = paths(pos, nums, ls, num)
-            if _paths:
-                pcs.extend([(p[0], p[1], num) for p in _paths])
-                print([(p[0], p[1], p[2] - 1) for p in pcs])
-            ls.append((n_pos, num))
-            return n_pos, ls, pcs
-    return pos, ls, pcs
-
+def is_next(a, b, h_map):
+    return h_map[b] - h_map[a] == 1
 
 def solve(prob):
-    ress = 0
-    nums = {}
-    for i in range(10):
-        nums[i] = find_num(prob, i)
+    height_map = get_heightmap(prob)
+    x0s = get_starts(prob)
+    return sum(walk(x0, height_map) for x0 in x0s)
 
-    for x0 in range(len(nums[0])):
-        res = []
-        ls = []
-        pcs = []
-        o_ls = [(-1, -1)]
-        pos = nums[0][x0]
-        i = 1
-        while ls != o_ls or len(pcs):
-            o_pos = [(-1, -1)]
-            while pos != o_pos and i < 10:
-                o_pos = pos
-                o_ls = ls.copy()
-                pos, ls, pcs = walk(pos, ls, nums[i], pcs, i)
-                if i == 9 and pos != o_pos:
-                    res.append(pos)
-                i += 1
-            if len(pcs):
-                pos, _, i = pcs.pop()
-                # print([(p[0][0], p[0][1] - 1) for p in pcs])
-            else:
-                break
-        ress += len(res) - 1
-    return ress
+def walk(pos, h_map, h=0):
+    sum = 0
+    if is_next(pos, (pos[0]+1, pos[1]), h_map):
+        sum += walk((pos[0]+1, pos[1]), h_map, h+1)
+    if is_next(pos, (pos[0]-1, pos[1]), h_map):
+        sum += walk((pos[0]-1, pos[1]), h_map, h+1)
+    if is_next(pos, (pos[0], pos[1]+1), h_map):
+        sum += walk((pos[0], pos[1]+1), h_map, h+1)
+    if is_next(pos, (pos[0], pos[1]-1), h_map):
+        sum += walk((pos[0], pos[1]-1), h_map, h+1)
+    if h == 9:
+        return 1
+    return sum
 
-
-# assert solve(three) == 3
-
-
-print(solve(thirteen))
-
-
-
-# assert solve(thirteen) == 13
-# assert solve(eightyone) == 81
-# assert solve(twotwoseven) == 227
-# print(solve(inp))
+print(solve(three),3)
+print(solve(thirteen),13)
+print(solve(eightyone),81)
+print(solve(twotwoseven),227)
+print(solve(inp))
