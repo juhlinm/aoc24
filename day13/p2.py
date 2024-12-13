d65c83c122462e5295c80cc23b3523569f243c90
MJUHLIN1 <mattias.juhlin@volvocars.com>
2024-12-13 11:34:52 +0100

    Day13

--- day13/p2.py
+++ day13/p2.py
@@ -1,42 +1,27 @@
-import math
-
 inp = list(map(lambda x: x.strip(), open("day13/inp.txt", "r").read().split("\n\n")))
 test = list(map(lambda x: x.strip(), open("day13/test.txt", "r").read().split("\n\n")))
-
-press_max = 10**12
 
 
 def add(v1, v2):
     return tuple([v1[0] + v2[0], v1[1] + v2[1]])
 
 
-def subtract(v1, v2):
-    return tuple([v1[0] - v2[0], v1[1] - v2[1]])
-
-
-def multiply(v, k):
-    return tuple([v[0] * k, v[1] * k])
-
-
-def distance(v1, v2):
-    diff = subtract(v1, v2)
-    return math.sqrt(diff[0] ** 2 + diff[1] ** 2)
+def determinant(v1, v2):
+    return v1[0] * v2[1] - v1[1] * v2[0]
 
 
 def press(A, B, prize):
-    for i in range(1, press_max + 1, 10**9):
-        print(i)
-        for j in range(1, press_max + 1, 10**9):
-            p1 = add((0, 0), multiply(B, j))
-            p2 = subtract(prize, multiply(A, i))
-            if distance(p1, p2) < 10**12:
-                break
-    print(i, j, p1, p2)
-    for i in range(i, press_max + 1):
-        for j in range(j, press_max + 1):
-            if add((0, 0), multiply(B, j)) == subtract(prize, multiply(A, i)):
-                return (i, j)
-    return None
+    det = determinant(A, B)
+    if det == 0:
+        return None
+    else:
+        a = (prize[0] * B[1] - prize[1] * B[0]) / det
+        b = (prize[1] * A[0] - prize[0] * A[1]) / det
+
+        if a % 1 == 0.0 and b % 1 == 0.0:
+            return (a, b)
+        else:
+            return None
 
 
 def solve(machines):
@@ -49,10 +34,9 @@ def solve(machines):
         prize = add(prize, (10**13, 10**13))
         res = press(A, B, prize)
         if res:
-            sum += 3 * res[0] + 1 * res[1]
-
+            sum += int(3 * res[0] + 1 * res[1])
     return sum
 
 
-print(solve(test), 480)
-# print(solve(inp))
+print(solve(test), 875318608908)
+print(solve(inp))
