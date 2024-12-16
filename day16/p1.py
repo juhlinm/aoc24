7b4522b14749372ec928774d276d538389ec0f7b
Mattias Juhlin <mattias@juhlin.de>
2024-12-16 20:20:30 +0100

    Day16 WIP

--- day16/p1.py
+++ day16/p1.py
@@ -1,68 +1,68 @@
+from functools import cache
+
 inp = open("day16/inp.txt", "r").readlines()
 inp_11048 = open("day16/inp_11048.txt", "r").readlines()
 inp_7036 = open("day16/inp_7036.txt", "r").readlines()
 
-
-def find_path(walls, end, walked, rotated, pos, dir):
+@cache
+def find_paths(walls, end, walked, rotated, pos, dir):
+    global min_score
     if pos == end:
         return walked, rotated, True
 
-    if dir == 1:
-        n_x = pos[0] + 1
-        n_y = pos[1]
-    elif dir == 2:
-        n_x = pos[0]
-        n_y = pos[1] + 1
-    elif dir == 3:
-        n_x = pos[0] - 1
-        n_y = pos[1]
-    elif dir == 0:
-        n_x = pos[0]
-        n_y = pos[1] - 1
+    poses = [(pos[0], pos[1] - 1), (pos[0] + 1, pos[1]), (pos[0], pos[1] + 1), (pos[0] - 1, pos[1])]
 
-    n_pos = (n_x, n_y)
-    t1 = (pos, (dir, (dir + 1) % 4))
-    t2 = (pos, (dir, (dir + 2) % 4))
-    t3 = (pos, (dir, (dir - 1) % 4))
+    found = False
+    r_found = False
+    r_walked = tuple()
+    r_rotated = tuple()
+    for i, p in enumerate(poses):
+        if abs(dir - i) == 2:
+            continue
 
-    if n_pos in walls:
-        if pos == (3, 9):
-            apa = 1
-        for t in [t1, t2, t3]:
-            if (t, False) not in rotated:
-                n_dir = t[1][1]
-                print(dir, n_dir)
-                walked, rotated, found = find_path(walls, end, walked, rotated, pos, n_dir)
-                if found:
-                    return walked, rotated, found
-                else:
-                    rotated.append(t, False)
-                    return walked, rotated, False
-    else:
-        walked.append((pos, n_pos))
-        print((pos, n_pos))
-        return find_path(walls, end, walked, rotated, n_pos, dir)
+        if p not in walls and p not in walked:
+            o_walked = walked
+            o_rotated = rotated
 
+            walked = walked + (p,)
+            
+            if abs(dir-i) in [1,3]:
+                rotated = rotated + ((p, i),)
+         
+            walked, rotated, found = find_paths(walls, end, walked, rotated, p, i)
+            if found:
+                score = 1000 * len(rotated) + len(walked)
+                if score < min_score and end in walked:
+                    min_score = score
+                    r_walked = walked
+                    r_rotated = rotated
+                r_found = True
+            walked = o_walked
+            rotated = o_rotated
+            
+    return r_walked, r_rotated, r_found
 
 def solve(prob):
-    walked = []
-    rotated = []
-    walls = []
+    global min_score
+    walked = tuple()
+    rotated = tuple()
+    walls = tuple()
     for y, line in enumerate(prob):
         for x, c in enumerate(line):
             if c == "S":
                 start = (x, y)
             if c == "#":
-                walls.append((x, y))
+                walls = walls + ((x, y),)
             if c == "E":
                 end = (x, y)
 
     dir = 1
     pos = start
-    walked, rotated, _ = find_path(walls, end, walked, rotated, pos, dir)
-    return 1000 * len(rotated) + len(walked)
+    min_score = 10**9
+    find_paths(walls, end, walked, rotated, pos, dir)
+    return min_score
 
 
 print(solve(inp_7036), 7036)
-# print(solve(inp_11048), 11048)
-# print(solve(inp))
+print(solve(inp_11048), 11048)
+print(solve(inp))
