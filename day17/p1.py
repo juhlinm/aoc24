736b28ddae401256d5954f87d4ce042c89d1201e
MJUHLIN1 <mattias.juhlin@volvocars.com>
2024-12-17 15:22:18 +0100

    Day17 p2 WIP

---
+++ day17/p1.py
@@ -0,0 +1,100 @@
+inp = open("day17/inp.txt", "r").read().split("\n\n")
+inp_p1 = open("day17/inp_p1.txt", "r").read().split("\n\n")
+
+
+def get_comb(regs, comb):
+    if comb < 4:
+        return comb
+    if comb == 4:
+        return regs[0]
+    if comb == 5:
+        return regs[1]
+    if comb == 6:
+        return regs[2]
+    else:
+        raise Exception("7 not valid")
+
+
+def adv(regs, comb):
+    c = get_comb(regs, comb)
+    regs[0] = regs[0] // (2**c)
+
+
+def bxl(regs, comb):
+    regs[1] = regs[1] ^ comb
+
+
+def bst(regs, comb):
+    c = get_comb(regs, comb)
+    regs[1] = c % 8
+
+
+def jnz(regs, comb):
+    if regs[0] == 0:
+        return None
+    return comb
+
+
+def bxc(regs, comb):
+    regs[1] = regs[1] ^ regs[2]
+
+
+def out(regs, comb, outp):
+    c = get_comb(regs, comb)
+    outp.append(c % 8)
+
+
+def bdv(regs, comb):
+    c = get_comb(regs, comb)
+    regs[1] = regs[0] // (2**c)
+
+
+def cdv(regs, comb):
+    c = get_comb(regs, comb)
+    regs[2] = regs[0] // (2**c)
+
+
+def execute(i_0, ops, regs, outp):
+    for i in range(i_0 + 1, len(ops), 2):
+        op = ops[i - 1]
+        comb = ops[i]
+
+        if op == 0:
+            adv(regs, comb)
+        elif op == 1:
+            bxl(regs, comb)
+        elif op == 2:
+            bst(regs, comb)
+        elif op == 3:
+            i = jnz(regs, comb)
+            if i is not None:
+                execute(i, ops, regs, outp)
+        elif op == 4:
+            bxc(regs, comb)
+        elif op == 5:
+            out(regs, comb, outp)
+        elif op == 6:
+            bdv(regs, comb)
+        elif op == 7:
+            cdv(regs, comb)
+
+
+def solve(prob):
+    regs, ops = prob
+    regs = regs.split("\n")
+    A = int(regs[0].split(": ")[1])
+    B = int(regs[1].split(": ")[1])
+    C = int(regs[2].split(": ")[1])
+    regs = [A, B, C]
+    ops = ops.split(": ")[1]
+    ops = list(map(int, ops.split(",")))
+    outp = []
+
+    i = 0
+    execute(i, ops, regs, outp)
+
+    return ",".join(list(map(str, outp)))
+
+
+print(f"{solve(inp_p1)}\n4,6,3,5,6,3,5,2,1,0")
+print(solve(inp))
