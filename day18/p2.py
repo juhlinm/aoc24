0abcf70370c9312988e1bd74a2136678caf9ef77
MJUHLIN1 <mattias.juhlin@volvocars.com>
2024-12-18 17:17:30 +0100

    Day18 pt2 WIP

--- day18/p2.py
+++ day18/p2.py
@@ -1,5 +1,21 @@
+from copy import deepcopy
+
 inp_test = open("day18/inp_test.txt", "r").readlines()
 inp = open("day18/inp.txt", "r").readlines()
+
+
+def is_reachable(unexplored, pos, end):
+    a = deepcopy(unexplored)
+    if pos == end:
+        return True
+
+    for x, y in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+        if (pos[0] + x, pos[1] + y) in a:
+            del a[(pos[0] + x, pos[1] + y)]
+            if is_reachable(a, (pos[0] + x, pos[1] + y), end):
+                return True
+
+    return False
 
 
 def get_paths(unexplored, explore):
@@ -21,26 +37,37 @@ def minimum(unexplored):
 def solve(prob, end):
     corrupted = []
 
-    for line in prob[0:12]:
+    for line in prob[0:1024]:
         x, y = map(int, line.split(","))
         corr = (x, y)
         corrupted.append(corr)
 
-    for line in prob[12:]:
+    for line in prob[1024:]:
         x, y = map(int, line.split(","))
         corr = (x, y)
+        corrupted.append(corr)
 
         unexplored = {}
         for y in range(end[1] + 1):
             for x in range(end[0] + 1):
                 if (x, y) not in corrupted:
                     unexplored[(x, y)] = float("inf")
+        c_un = deepcopy(unexplored)
 
-        corrupted.append(corr)
         start = (0, 0)
+
+        # if not is_reachable(c_un, start, end):
+        #     return corr
+
+        abc = {}
+        if corr == (6, 1):
+            stop = 1
+
         unexplored[start] = 0
         while len(unexplored) != 0:
             explore = minimum(unexplored)
+            # if not is_reachable(c_un, start, explore):
+            #     return corr
             if explore == end:
                 break
             else:
@@ -49,11 +76,12 @@ def solve(prob, end):
                     time = unexplored[explore] + 1
                     if time < unexplored[path]:
                         unexplored[path] = time
-
+            if unexplored[explore] == float("inf"):
+                return corr
             del unexplored[explore]
 
     return None
 
 
 print(solve(inp_test, (6, 6)), (6, 1))
-# print(solve(inp, (70, 70)))
+print(solve(inp, (70, 70)))
