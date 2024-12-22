b0fb7430719cc4ffb87270c8b474ac23c9a60ade
juhlinm <mattias@juhlin.de>
2024-12-22 17:41:12 +0100

    Day19

--- day19/p2.py
+++ day19/p2.py
@@ -1,20 +1,8 @@
-import re
-from collections import defaultdict
 from functools import cache
 
 inp_test = open("day19/inp_test.txt", "r").read().split("\n\n")
+inp_6 = open("day19/inp_6.txt", "r").read().split("\n\n")
 inp = open("day19/inp.txt", "r").read().split("\n\n")
-
-def get_idxs(towel, pattern):
-    matches = re.finditer(towel, pattern)
-    return [match.start() for match in matches]
-
-
-def viable(tuples_list, end):
-    graph = defaultdict(set)
-    for a, b in tuples_list:
-        graph[a].add(b)
-    return 0
 
 
 def solve(prob):
@@ -22,16 +10,20 @@ def solve(prob):
     towels = [x.strip() for x in towels.split(",")]
     patterns = [x.strip() for x in patterns.split("\n")]
 
-    s = 0
-    for pattern in patterns:
-        print(pattern)
-        fills = []
-        for towel in towels:
-            idxs = get_idxs(towel, pattern)
-            for idx in idxs:
-                fills.append((idx, idx + len(towel)))
-        s += viable(fills, len(pattern))
-    return s
+    @cache
+    def count_builds(pattern):
+        if len(pattern) == 0:
+            return 1
+
+        return sum(
+            count_builds(pattern.removeprefix(p))
+            for p in towels
+            if pattern.startswith(p)
+        )
+
+    return sum(list(map(count_builds, patterns)))
+
 
 print(solve(inp_test), 16)
-print(solve(inp), 752461716635602) # prob right
+print(solve(inp_6), 6)
+print(solve(inp))
